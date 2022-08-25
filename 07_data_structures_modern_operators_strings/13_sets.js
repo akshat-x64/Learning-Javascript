6c2cc72a9859c2048bb8450e4a7dcfdea1818a71
Akshat Dwivedi <93469027+akshat-x64@users.noreply.github.com>
2022-08-25 18:41:48 +0530

    Delete 13_sets.js

--- 07_data_structures_modern_operators_strings/13_sets.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// sets is collection of unique values and it does not have repeating values
-const orderSet = new Set(["pizza", "pasta", "pasta", "disotta", "pizza"]);
-console.log(orderSet);
