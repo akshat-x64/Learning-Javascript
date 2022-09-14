57576666c3eb9809de51e8ee6017194fcc6f64e2
Akshat Dwivedi <93469027+akshat-x64@users.noreply.github.com>
2022-09-14 13:49:50 +0530

    Add files via upload

---
+++ 03_remainder_operaotr.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log(8 % 3);
+console.log(41 % 3);
+console.log(87 % 3);
+console.log(854 % 3);
+console.log(8222 % 3);
+//program to find even or odd
+const evenOdd = (num) => (num % 2 === 0 ? "Even" : "False");
+console.log(evenOdd(12));
