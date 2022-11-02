29934fc636fc65773109e72ac2c522a64a0cd69f
Akshat Dwivedi <93469027+akshat-x64@users.noreply.github.com>
2022-11-02 21:05:25 +0530

    Update 02_passing_arguments_value_vs_reference.js

--- 08_closer_look_at_function/02_passing_arguments_value_vs_reference.js
+++ 08_closer_look_at_function/02_passing_arguments_value_vs_reference.js
@@ -9,13 +9,23 @@ const akshat = {
 const checkIn = function (flightNum, passenger) {
   passenger.name = "Mr." + passenger.name;
   flightNum = "LH999";
-  if ((passenger.passport = 123456789123456)) {
+  if (passenger.passport === 123456789123456) {
     console.log("Checked in");
   } else {
-    console.log("Checked out");
+    console.log("Checked in not allowed");
   }
 };
 
 checkIn(flight, akshat);
 console.log(flight);
 console.log(akshat);
+
+//this is thesame as doing
+const flightNum = flight;
+const passenger1 = akshat;
+const newPassport = function (person) {
+  person.passport = 10000000000000;
+};
+newPassport(passenger1);
+
+checkIn(flight, akshat);
